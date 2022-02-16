68676e793a21e1e3ca13e88506351fffb11383aa
이진솔 <jinsol9419@naver.com>
2022-02-16 15:31:06 +0900

    Add board contents post feature

--- dailyRetro/src/main/java/com/retro/dailyRetro/board/entity/Board.java
+++ dailyRetro/src/main/java/com/retro/dailyRetro/board/entity/Board.java
@@ -1,16 +1,12 @@
 package com.retro.dailyRetro.board.entity;
 
 import lombok.Data;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity // DB Table
 @Data //lombok
+@Table(name ="board")
 public class Board {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer idx;
